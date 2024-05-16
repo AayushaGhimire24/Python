2be31b28d9d2e0c85dac7ff09a8cd0300f0a0a5b
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-05-16 07:30:19 +0545

    Add files via upload

---
+++ ifelse1.py
@@ -0,0 +1,4 @@
+#WAP to inout a number and check odd/even using  if else as ternary operator
+a=int(input("enter a number"))
+Msg='Even' if a%2==0 else 'Odd'
+print(Msg)
---
+++ ifelse2.py
@@ -0,0 +1,5 @@
+#Using if else as ternary operator WAP to inout two numbers and print the greater number
+a=int(input('enter first number '))
+b=int(input('Enter second number '))
+grt=a if a>b else b
+print (grt,"is greater")
