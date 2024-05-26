f7e74a875660f922a6aaaff3c67407eba5d8b8a1
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-05-26 20:11:16 +0545

    Add files via upload

---
+++ StringFunctions.py
@@ -0,0 +1,8 @@
+txt="this is python"
+print(txt.capitalize())#converts first word into capital
+
+txt='Hello'
+print(txt.count('l'))#Returns the total no.of times a word occurs in the given string
+
+txt='Hello'
+print(txt.replace('o','i'))
