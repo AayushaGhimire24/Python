b4c4b2c55c4799129ab53a6009f794883b58db18
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-05-26 20:00:33 +0545

    Add files via upload

---
+++ StringFormatting1.py
@@ -0,0 +1,21 @@
+#By using f-string
+#f-string with a variable
+age=20
+txt=f"Ram is {age} years old"
+print(txt)
+
+#f-string with a function
+x=50.89
+txt=f"Rounded value is {round(x)}"
+print(txt)
+
+#f-string with operation
+a=5
+b=7
+txt=f"Multiplication result={a*b}"
+print(txt)
+
+#f-string with modifier
+price=85
+txt=f"Price={price:.2f} rupees"
+print(txt)
