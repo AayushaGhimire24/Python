c0ca23a87173f35c01be9e241209b3dcdecbb5eb
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-05-27 18:21:34 +0545

    Add files via upload

---
+++ ListAscending.py
@@ -0,0 +1,10 @@
+#WAP to input name of 5 countries and sort them in ascending order
+countries=input("Enter the name of 5 countries: ").split()
+countries.sort()
+print(countries)
+
+
+#WAP to input name of 5 countries and sort them in descending order
+countries=input("Enter the name of 5 countries: ").split()
+countries.sort(reverse=True)
+print(countries)
