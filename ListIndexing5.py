76206bdf6f9a33a166cf038a42ff7a9f98e07cf4
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-05-28 19:08:23 +0545

    Add files via upload

---
+++ ListIndexing5.py
@@ -0,0 +1,4 @@
+#WAP to input a list of 5 countries and print them from last to first
+countries=input("Enter the name of 5 countries").split()
+country=countries[::-1]
+print(country)
---
+++ ListModifying6.py
@@ -0,0 +1,17 @@
+myList=["apple","orange","grapes","papaya"]
+myList[1]="melon"
+print(myList)
+
+myList=["apple","orange","grapes"]
+myList[1:3]=["mango","litchi"]
+print(myList)
+
+
+myList=["apple","orange","grapes"]
+myList[1:2]=["mango","litchi"]
+print(myList)
+
+
+myList=["apple","orange","grapes"]
+myList[1:3]=["mango"]
+print(myList)
