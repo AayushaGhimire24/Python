245bc26260a6e848fe7a244a73e41f21f2adc61d
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-05-29 17:35:59 +0545

    Add files via upload

---
+++ ListRemoving11.py
@@ -0,0 +1,18 @@
+#using remove() method:
+nums=[1,2,3,4,4,2]
+nums.remove(2)#Removes first occurance
+print(nums)
+
+#Using pop() method:
+alpha=['a','b','c','d']
+alpha.pop(1)
+print(alpha)
+
+#Using del keywords:
+alpha=['a','b','c','d']
+del alpha[1]
+print(alpha)
+
+# nums=[1,2,3]
+# del nums
+# print(nums)#->generates error because sav delete vayisakeko hunxa
