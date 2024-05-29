a9f659a9a2436451a0074700a5217f69d4fee7a5
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-05-29 17:35:41 +0545

    Add files via upload

---
+++ ListAddingItems10.py
@@ -0,0 +1,16 @@
+#Adding items By using functions:
+#Append()
+fruits=["Mango","Litchi"]
+fruits.append("Cherry")
+print(fruits)
+
+#insert
+fruits=["Mango","Litchi"]
+fruits.insert(1,"Orange")
+print(fruits)
+
+#extend()
+fruits=["Mango","Litchi"]
+veg=["Cabbage","Cauli"]
+fruits.extend(veg)
+print(fruits)
