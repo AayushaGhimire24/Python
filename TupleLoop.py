4d7bbc50014066ede2933ef9483037aefcef9db9
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-06-02 17:49:55 +0545

    Add files via upload

---
+++ TupleLoop.py
@@ -0,0 +1,16 @@
+#EG:.1
+fruits=('Apple','Mango','Orange')
+for fruit in fruits:
+    print(fruit)
+
+#EG:.2
+fruits=('Apple','Mango','Avocado')
+for i in range(len(fruits)):
+    print(fruits[i])
+
+#EG:.3
+fruits=('Apple','Mango','Kiwi')
+i=0
+while i<len(fruits):
+  print(fruits[i])
+  i+=1
