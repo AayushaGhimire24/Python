b22e53f8ed492b827df6710f83849abeb23a8c43
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-06-03 18:09:42 +0545

    Add files via upload

---
+++ FunctionOddEven.py
@@ -0,0 +1,9 @@
+#WAP to input a number, pass it to a function that checks odd/even.
+def oddeven(a):
+    if a%2==0:
+        print("Even")
+    else:
+        print("odd")
+a=int(input("Enter a number:"))
+oddeven(a)    
+    
---
+++ FunctionSum.py
@@ -0,0 +1,13 @@
+#Create a function that takes two values and print their sum
+def add(a,b):
+    c=a+b
+    print("sum=",c)
+add(5,6)
+
+#Taking input from user
+def add(a,b):
+    c=a+b
+    print("sum=",c)
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+add(a,b)
