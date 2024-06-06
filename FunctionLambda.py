45f869f8ca6c5b8648f120c0d589e709e41ea931
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-06-06 16:50:24 +0545

    Add files via upload

---
+++ FunctionLambda.py
@@ -0,0 +1,7 @@
+#EG:.1
+x=lambda a:a+5
+print(x(5))
+
+#EG:.2
+x=lambda a,b:a+b
+print(x(5,8))
---
+++ FunctionPositionalArguments.py
@@ -0,0 +1,4 @@
+def myfunction(a,b,/):
+    print(a,b)
+myfunction(2,3)
+myfunction(a=5,b=10)#Error aauxa esma chai
