16207668376e89be0b664ec03a3bf4d15734be83
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-06-28 18:53:23 +0545

    Update GetterSetter.py

--- GetterSetter.py
+++ GetterSetter.py
@@ -4,13 +4,13 @@
 class Car:
     def set_model(self,model):
         self.__model=model
-    @property
+ 
     def get_model(self):
         return self.__model
     
     def set_price(self,price):
         self.__price=price
-    @property
+  
     def get_price(self):
         return self.__price
 c=Car()
