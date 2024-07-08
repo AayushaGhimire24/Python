42a5079fee000fdbe31d38bc95c40706ff6f2f90
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-07-08 17:25:52 +0545

    Add files via upload

---
+++ Sample.py
@@ -0,0 +1,5 @@
+sample_variable="This is a string variable in the Sample.py module"
+def say_hello(name):
+   return f"Hello,{name} welcome to this simple module"
+def add(a,b):
+   return f"The sum of {a}+{b} is={a+b}"
